bde17feeb18ebc832364332d0ed5dc3540772258
dengwenhao <dengwenhao@pcitech.com>
2020-10-13 20:05:11 +0800

    10.13

--- SpringDemo/src/main/java/com/dwh/springdemo/controller/SwaggerController.java
+++ SpringDemo/src/main/java/com/dwh/springdemo/controller/SwaggerController.java
@@ -2,36 +2,42 @@ package com.dwh.springdemo.controller;
 
 import com.dwh.springdemo.response.BaseResponse;
 import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpServer;
 import io.swagger.annotations.*;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.spring.web.json.Json;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * swagger标签演示类
  * @author: dwh
  * @DATE: 2020/9/22
  */
-@Api(tags = "Swagger测试用")
+@Api(tags = "Swagger测试用")//Api标签用来标注该类是用swagger文档
 @RestController
 @RequestMapping("Swagger")
 public class SwaggerController {
 
-    @ApiOperation(value = "测试swagger的方法一", notes = "备注说明")
+    /**ApiOperation标签用来说明这个方法使用swagger文档说明**/
+    @ApiOperation(value = "演示swagger标签使用的方法一", notes = "备注说明")
+    /**ApiImplicitParams用来说明请求中包含的参数**/
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "password", value = "密码", required = true, paramType = "query", defaultValue = "123"),
-            @ApiImplicitParam(name = "age", value = "年龄", required = true, paramType = "header")
+            @ApiImplicitParam(name = "query", value = "query类型的参数", required = true, paramType = "query", dataType = "String" , defaultValue = "123"),
+            @ApiImplicitParam(name = "header", value = "header类型的参数", required = true, paramType = "header"),
+            @ApiImplicitParam(name = "path", value = "path类型的参数", required = true),
+            @ApiImplicitParam(name = "body", value = "body类型的参数", required = true),
+            @ApiImplicitParam(name = "form", value = "form类型的参数", required = true)
     })
-    @RequestMapping("/Test")
+    @RequestMapping(value = "/Test", method = RequestMethod.POST)
     @ApiResponses({
             @ApiResponse(code = 200, message = "成功", response = BaseResponse.class),
             @ApiResponse(code = 400, message = "失败")
     })
-    public String test(@RequestParam String password, @RequestHeader String age){
+    public String test(@RequestParam String age){
         Map<String, Object> map = new HashMap<>(2);
         map.put("code", "0");
         map.put("message", "success");
